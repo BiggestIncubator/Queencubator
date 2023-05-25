b2b96bd51cef539d4fc34304590a0f074d114536
infectiousoma <d.benniv@comcast.net>
2023-05-25 13:32:10 +0700

    docker

--- launch.sh
+++ launch.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-<<<<<<< HEAD
-for f in scripts/*.sh
-=======
 
 script_dir=$(dirname "$0")
+pids=()
+
+# Function to kill spawned processes
+cleanup() {
+    for pid in "${pids[@]}"; do
+        kill "$pid" 2> /dev/null
+    done
+}
+
+# Trap termination signals and call cleanup function
+trap cleanup SIGINT SIGTERM SIGHUP
 
 for f in "$script_dir"/scripts/*.sh
->>>>>>> origin
 do
     if [ -f "$f" ]; then
         chmod +x "$f"
-        konsole -e "$f" &
+        "$f" &
+        pids+=("$!")
     fi
 done
+
+# Wait for all spawned processes to finish
+wait
