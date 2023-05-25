10b4c9895d6d98d626f05121996a73bd46151b16
infectiousoma <d.benniv@comcast.net>
2023-05-25 16:44:43 +0700

    made secrets folder for api keys, scripts pull relevent data from secrets

--- scripts/moodgpt.sh
+++ scripts/moodgpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export OPENAI_API_KEY=
-export TELEGRAM_BOT_TOKEN=
 export PERSONA=moodgpt
+export OPENAI_API_KEY= $(grep "OPENAI" scripts/secrets/${PERSONA}.env)
+export TELEGRAM_BOT_TOKEN= $(grep "TELEGRAM" scripts/secrets/${PERSONA}.env)
 
 python3 main.py
