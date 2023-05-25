55de3ab02ec3fa979b7fa20aa68cfc9584e9fa3e
infectiousoma <d.benniv@comcast.net>
2023-05-25 19:56:04 +0700

    cleaned up

--- scripts/internknight.sh
+++ scripts/internknight.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PERSONA=internknight
-export OPENAI_API_KEY=
-export TELEGRAM_BOT_TOKEN=
+export PERSONA=queencubator
+export OPENAI_API_KEY= $(grep "OPENAI" scripts/secrets/${PERSONA}.env)
+export TELEGRAM_BOT_TOKEN= $(grep "TELEGRAM" scripts/secrets/${PERSONA}.env)
 
 python3 main.py
