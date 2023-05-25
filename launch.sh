04119a675d8039cfcd37dbad879585831308a4c5
LaDoger <60722582+LaDoger@users.noreply.github.com>
2023-05-25 13:14:25 +0700

    launch.sh now runs all processes in a single terminal

--- launch.sh
+++ launch.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 script_dir=$(dirname "$0")
+pids=()
+
+# Function to kill spawned processes
+cleanup() {
+    for pid in "${pids[@]}"; do
+        kill "$pid" 2> /dev/null
+    done
+}
+
+# Trap termination signals and call cleanup function
+trap cleanup SIGINT SIGTERM SIGHUP
 
 for f in "$script_dir"/scripts/*.sh
 do
     if [ -f "$f" ]; then
         chmod +x "$f"
-        konsole -e "$f" &
+        "$f" &
+        pids+=("$!")
     fi
 done
+
+# Wait for all spawned processes to finish
+wait
