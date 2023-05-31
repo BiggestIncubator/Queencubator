96a9615ab80fb09cc6d94176e47fb1bba8966ed0
LaDoger <60722582+LaDoger@users.noreply.github.com>
2023-05-31 10:54:46 +0700

    strip

--- personae/queencubator/filterer.py
+++ personae/queencubator/filterer.py
@@ -3,6 +3,6 @@
 def filterer(ai_output:str) -> str:
 
     import re
-    reply = re.sub(r'#\w+', '', ai_output) # remove hashtags
+    reply = re.sub(r'#\w+', '', ai_output).strip() # remove hashtags
 
     return reply
