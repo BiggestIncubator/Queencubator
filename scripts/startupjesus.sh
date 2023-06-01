6067a9f2c131f46270fe8e135bc87b004c7209e3
infectiousoma <d.benniv@comcast.net>
2023-06-01 17:38:43 +0700

    setup script for automating bot deployment

---
+++ scripts/startupjesus.sh
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+#!/bin/bash
+
+export PERSONA=startupjesus
+export OPENAI_API_KEY= $(grep "OPENAI" scripts/secrets/${PERSONA}.env)
+export TELEGRAM_BOT_TOKEN= $(grep "TELEGRAM" scripts/secrets/${PERSONA}.env)
+
+python3 main.py
+=======
+#!/bin/bash
+
+export OPENAI_API_KEY=
+export TELEGRAM_BOT_TOKEN=
+export PERSONA=startupjesus
+
+python3 main.py
+>>>>>>> origin
