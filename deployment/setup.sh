dfab2cf68b2954cb3456d1455e491fabef624872
infectiousoma <d.benniv@comcast.net>
2023-06-14 12:09:50 +0700

    CI scripts and workflow files

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-cd "$(dirname "$0")."
+#cd "$(dirname "$0")."
 
 # Prints the header for the docker compose file and override file
 printf "version: '3.7'\nservices:\n" > docker-compose.yaml
@@ -12,19 +12,13 @@ printf 'services:\n' > docker-compose.override.yaml
 # FILE=$(ls scripts/secrets/*.env)
 FILE=$(cat newbot.txt)
 
-# Loop that removes the path to the script and the .sh producing a list of filenames
-for LIST in $FILE; do
-  X=$LIST;
-  Y=${X%.env};
-  BOT=${Y##*/};
-
 # Loop that generates entries in the compose file and override file for each bot
 # Generates launch script for each bot if a key file exists in scripts/secrets
-    for FILENAME in $BOT; do
-      export PERSONAE=$FILENAME;
-      cat templates/docker-compose.template | envsubst >>  docker-compose.yaml;
-      cat templates/docker-compose.override.template | envsubst >> docker-compose.override.yaml;
-      cat templates/botscript.template | envsubst > scripts/"$FILENAME".sh;
-      chmod +x scripts/*.sh
-    done
+for FILENAME in $FILE; do
+   export PERSONAE=$FILENAME;
+   cat templates/docker-compose.template | envsubst >>  docker-compose.yaml;
+   cat templates/docker-compose.override.template | envsubst >> docker-compose.override.yaml;
+   cat templates/botscript.template | envsubst > scripts/"$FILENAME".sh;
+   chmod +x scripts/*.sh
 done
+
